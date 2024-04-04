32dc146d994975df68a70d08c8b49e0c638787f9
mv.pranay <mvpranavy@gmail.com>
2024-04-04 20:42:09 +0530

    cell.py: Added break wall functions for each indiv direction

--- cell.py
+++ cell.py
@@ -1,5 +1,6 @@
 class Cell:
     def __init__(self,x,y):
+        # x and y refer to cell numbers, not actual coords
         self.x = x
         self.y = y
         # store if a wall exists in a certain direction immediately or not
@@ -13,3 +14,16 @@ class Cell:
         self.down = False if down else True
         self.right = False if right else True
         self.left = False if left else True
+
+    # these functions don't care if the wall is already broken
+    def break_up_wall(self):
+        self.up = False
+
+    def break_down_wall(self):
+        self.down = False
+    
+    def break_left_wall(self):
+        self.left = False
+
+    def break_right_wall(self):
+        self.right = False
